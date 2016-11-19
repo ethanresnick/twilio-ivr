f573ee4c6ff39e72a579a287ead0b0cc32ebdfb2
Ethan Resnick <ethan.resnick@gmail.com>
2016-11-19 01:42:03 -0800

    Tests: make sure returned express app understands TwimlResponse objects

---
+++ test/integration/understandsTwimlResponseSpec.ts
@@ -0,0 +1,32 @@
+import twilio = require("twilio");
+import request = require("supertest");
+import lib from "../../lib/";
+import { UsableState } from "../../lib/state";
+
+let states: UsableState[] = [<UsableState>{
+  uri: "/returns-twiml-response",
+  name: "DUMMY",
+  isEndState: true,
+  twimlFor() { return new twilio.TwimlResponse(); }
+}, <UsableState>{
+  uri: "/returns-twiml-string",
+  name: "DUMMY",
+  isEndState: true,
+  twimlFor() { return 'Test'; }
+}];
+
+describe("rending Twiml", () => {
+  let app = lib(states, { twilio: { authToken: "", validate: false } });
+
+  it("should render TwimlResponse object", () => {
+    return request(app)
+      .post("/returns-twiml-response")
+      .expect('<?xml version="1.0" encoding="UTF-8"?><Response></Response>');
+  });
+
+  it("should still be able to render strings", () => {
+    return request(app)
+      .post("/returns-twiml-string")
+      .expect('Test');
+  });
+});
