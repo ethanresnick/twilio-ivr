d5bfa99952314efeb17aee83a923cae6f13fd4b8
Ethan Resnick <ethan.resnick@gmail.com>
2016-11-22 03:26:00 -0800

    Add handwritten types now that 1.0 api is mostly set

---
+++ lib/index.d.ts
@@ -0,0 +1,126 @@
+/// <reference types="express" />
+/// <reference types="twilio" />
+import { Express, Handler } from "express";
+import { TwimlResponse } from "twilio";
+
+export default function (states: UsableState[], config: config): Express;
+
+export type config = {
+  express?: any;
+  twilio: {
+    authToken: string;
+    validate?: boolean;
+  };
+  staticFiles?: {
+    path: string;
+    mountPath?: string;
+    holdMusic?: {
+      path: string;
+      loopCount?: number;
+      endpoint?: string;
+    };
+    middleware?: Handler;
+  };
+};
+
+export type urlFor = (path: string, options?: UrlForOptions) => string;
+export type UrlForOptions = {
+  query?: any;
+  fingerprint?: boolean;
+  absolute?: boolean;
+};
+
+export type UsableState = BranchingState | EndState | NormalState | AsynchronousState;
+export type State = UsableState | RenderableState | RoutableState;
+
+export interface AbstractState {
+  name: string;
+}
+
+export interface RoutableState extends AbstractState {
+  uri: string;
+}
+
+export interface BranchingState extends AbstractState {
+  transitionOut(inputData?: CallDataTwiml): Promise<UsableState> | UsableState;
+}
+
+export interface RenderableState extends AbstractState {
+  twimlFor(urlFor: urlFor, inputData?: CallDataTwiml): TwimlResponse | string;
+}
+
+export interface EndState extends RenderableState {
+  isEndState: true;
+}
+
+export interface AsynchronousState extends RenderableState {
+  backgroundTrigger(urlFor: urlFor, inputData?: CallDataTwiml): void;
+}
+
+export interface NormalState extends BranchingState, RenderableState {
+  processTransitionUri: string;
+}
+
+type CallDirection = "inbound" | "outbound-api" | "outbound-dial" | "trunking-terminating" | "trunking-originating";
+type CallStatus = "queued" | "ringing" | "in-progress" | "busy" | "failed" | "canceled" | "no-answer" | "completed";
+
+export interface CallDataTwiml {
+  CallSid: string;
+  Direction: CallDirection;
+  CallStatus: CallStatus;
+  AccountSid: string;
+  ForwardedFrom?: string;
+  CallerName?: string;
+  To: string;
+  ToCountry?: string;
+  ToState?: string;
+  ToCity?: string;
+  ToZip?: string;
+  From: string;
+  FromCountry?: string;
+  FromState?: string;
+  FromCity?: string;
+  FromZip?: string;
+  Called: string;
+  CalledCountry: string;
+  CalledState: string;
+  CalledCity: string;
+  CalledZip: string;
+  Caller: string;
+  CallerCountry: string;
+  CallerState: string;
+  CallerCity: string;
+  CallerZip: string;
+  ApiVersion: '2010-04-01';
+}
+
+export interface GatherDataTwiml extends CallDataTwiml {
+  msg: "Gather End";
+  Digits: string;
+}
+
+export interface CallDataAPI {
+  dateCreated: string | null;
+  dateUpdated: string | null;
+  startTime: string | null;
+  endTime: string | null;
+  duration: string | null;
+  parentCallSid: string | null;
+  toFormatted: string;
+  fromFormatted: string;
+  price: string | null;
+  priceUnit: string;
+  phoneNumberSid: string;
+  answeredBy: string | null;
+  forwardedFrom: string | null;
+  groupSid: string | null;
+  callerName: string | null;
+  uri: string;
+  sid: string;
+  direction: CallDirection;
+  status: CallStatus;
+  accountSid: string;
+  to: string;
+  from: string;
+  apiVersion: string;
+}
