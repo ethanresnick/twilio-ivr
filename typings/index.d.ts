aeccdfb57cdbbdd71b82b2adfac70312ad844461
Ethan Resnick <ethan.resnick@gmail.com>
2016-11-23 05:07:17 -0800

    Remove stream-equal augmentation typings
    The stream-equal package now bundles its typings directly.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,1 @@
 declare module "static-expiry";
-
-declare module "stream-equal" {
-  type Cb = (err: Error | null, equal: boolean) => void;
-
-  function streamEqual(stream1: NodeJS.ReadableStream, stream2: NodeJS.ReadableStream, cb: Cb): undefined;
-  function streamEqual(stream1: NodeJS.ReadableStream, stream2: NodeJS.ReadableStream): Promise<boolean>;
-  export= streamEqual;
-}
